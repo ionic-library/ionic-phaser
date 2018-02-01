5fd4a42fb0bd212948348dbc43cd241990ed0847
Iru Hernández <iru343@hotmail.com>
2018-02-01 19:42:28 +0100

    basic text-clicker

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,27 +1,49 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import "pixi";
+import "p2";
+import * as Phaser from "phaser-ce";
 
-import { Game } from '../../game/game';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  /**
-   * Game instance
-   * @private
-   * @type {Game}
-   * @memberof HomePage
-   */
-  private gameInstance: Game;
-
-  /**
-   * Creates an instance of HomePage.
-   * @param {NavController} navCtrl 
-   * @memberof HomePage
-   */
-  constructor( public navCtrl: NavController ) {
-    this.gameInstance = new Game();
+  private game:any;
+  private clicks= 0;
+	//private that:any;
+  constructor(public navCtrl: NavController) {
+  	//this.that = this;
   }
+
+  ionViewDidLoad(){
+  	this.game = new Phaser.Game('100%', '100%', Phaser.CANVAS, 'game0', { preload: this.preload.bind(this), create: this.create.bind(this), update: this.update.bind(this) });
+  }
+
+
+  preload(){
+  		
+  }
+
+  
+
+  create() {
+    this.game.stage.backgroundColor = '#e43a19';
+    let text = this.game.add.text(this.game.world.centerX, this.game.world.centerY, "click me", { font: "24px Arial", fill: "#f2f4f7", align: "center" });
+
+    text.anchor.set(0.5);
+
+    text.inputEnabled = true;
+    text.events.onInputDown.add(()=>{
+      this.clicks++;
+      text.text = "clicked " + this.clicks + " times";
+    }, this);
+    
+
+}
+ update() {
+   //console.log("its me update");
+}
+
 }

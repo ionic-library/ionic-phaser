123025002fe830bf0f0e773eef1deaa85eebdc1d
Iru Hernández <iru343@hotmail.com>
2018-02-06 23:20:01 +0100

    main state

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,63 +11,100 @@ import * as Phaser from "phaser-ce";
 })
 export class HomePage {
   private game:any;
-  PreloadState = {};
-  Level = {};
-  tennisball:any;
-  platform:any;
-  tapScreen = false;
-
-  constructor(public navCtrl: NavController) {
-    this.PreloadState = {
-      preload(){
-        this.game.load.image('platform', 'assets/imgs/platform.png');
-        this.game.load.image('ball', 'assets/imgs/ball.png');
-      },
-      create() {
-        this.game.stage.backgroundColor = '#3090a1';           
+  States = {
+    PreloadState: {
+        preload(){
+          this.game.load.image('platform', 'assets/imgs/platform.png');
+          this.game.load.image('ball', 'assets/imgs/ball.png');
+        },
+        create() {
+          this.game.stage.backgroundColor = '#3090a1';           
         },
         update() {
-        this.game.state.start('Level');
+          this.game.state.start('Menu');
         }
-    }
-
-    this.Level = {
-      preload(){
+    },
+    Menu: {
+        preload(){
+            this.game.load.bitmapFont('carrier_command', 'assets/fonts/bitmapFonts/carrier_command.png', 'assets/fonts/bitmapFonts/carrier_command.xml');
+            },
+        create() {
+          this.game.input.onTap.add((pointer)=>{
+              this.game.state.start("Level");
+          }, this);
+          this.timer = 0;
+          this.text = this.game.add.bitmapText(10, 100, 'carrier_command','Touch to Play Game !', 14);
           
-          },
-      create() {
-        this.game.input.onTap.add((pointer)=>{this.tapScreen =  this.tennisball.body.touching.down ? true:false;}, this);
-
-        this.platform =  this.game.add.sprite(0, this.game.world.bounds.height - 100,'platform');
-        this.game.physics.enable(this.platform, Phaser.Physics.ARCADE);
-        this.platform.scale.setTo(1, 3);
-        this.platform.width = this.game.world.bounds.width;
-        this.platform.body.enable = true;
-        this.platform.body.immovable = true;
-
-        this.tennisball =  this.game.add.sprite(this.game.world.centerX,this.game.world.centerY,'ball');
-        this.game.physics.enable(this.tennisball, Phaser.Physics.ARCADE);
-        this.tennisball.scale.setTo(0.75, 0.75);
-        this.tennisball.body.enable = true;
-        this.tennisball.body.gravity.y = 100;
-        this.tennisball.body.bounce.y = 0.4;
-        this.tennisball.body.collideWorldBounds = true;
+          this.platform =  this.game.add.sprite(0, this.game.world.bounds.height - 100,'platform');
+          this.game.physics.enable(this.platform, Phaser.Physics.ARCADE);
+          this.platform.scale.setTo(1, 3);
+          this.platform.width = this.game.world.bounds.width;
+          this.platform.body.enable = true;
+          this.platform.body.immovable = true;
+  
+          this.tennisball =  this.game.add.sprite(this.game.world.centerX,this.game.world.centerY,'ball');
+          this.game.physics.enable(this.tennisball, Phaser.Physics.ARCADE);
+          this.tennisball.scale.setTo(0.75, 0.75);
+          this.tennisball.body.enable = true;
+          this.tennisball.body.gravity.y = 100;
+          this.tennisball.body.collideWorldBounds = true;
         },
         update() {
-           this.game.physics.arcade.collide(this.tennisball, this.platform);
+             this.game.physics.arcade.collide(this.tennisball, this.platform);
+  
+             if(this.tennisball.body.touching.down){             
+                this.tennisball.body.velocity.y = -200;
+            }
 
-           if(this.tapScreen){             
-              this.tennisball.body.velocity.y = -200;
-              this.tapScreen = false;
-           }
+            this.timer += this.game.time.elapsed;
+            if(this.timer >= 400){
+                this.timer -= 400;
+                this.text.visible = !this.text.visible;
+            }           
+        }
+    },
+    Level: {
+        preload(){
+            
+            },
+        create() {
+          this.game.input.onTap.add((pointer)=>{this.tapScreen =  this.tennisball.body.touching.down ? true:false;}, this);
+  
+          this.platform =  this.game.add.sprite(0, this.game.world.bounds.height - 100,'platform');
+          this.game.physics.enable(this.platform, Phaser.Physics.ARCADE);
+          this.platform.scale.setTo(1, 3);
+          this.platform.width = this.game.world.bounds.width;
+          this.platform.body.enable = true;
+          this.platform.body.immovable = true;
+  
+          this.tennisball =  this.game.add.sprite(this.game.world.centerX, this.game.world.bounds.height - 150,'ball');
+          this.game.physics.enable(this.tennisball, Phaser.Physics.ARCADE);
+          this.tennisball.scale.setTo(0.75, 0.75);
+          this.tennisball.body.enable = true;
+          this.tennisball.body.gravity.y = 100;
+          this.tennisball.body.bounce.y = 0.2;
+          this.tennisball.body.collideWorldBounds = true;
+        },
+        update() {
+             this.game.physics.arcade.collide(this.tennisball, this.platform);
+  
+             if(this.tapScreen){             
+                this.tennisball.body.velocity.y = -200;
+                this.tapScreen = false;
+            }
         }
     }
   }
 
+  constructor(public navCtrl: NavController) {
+    
+  }
+
   ionViewDidLoad(){
        this.game = new Phaser.Game('100%', '100%', Phaser.CANVAS, 'game0');
-       this.game.state.add('PreloadState', this.PreloadState);
-       this.game.state.add('Level', this.Level);
+       this.game.state.add('PreloadState',  this.States.PreloadState);
+       this.game.state.add('Menu', this.States.Menu);
+       this.game.state.add('Level', this.States.Level);
        this.game.state.start('PreloadState');
   }
 
